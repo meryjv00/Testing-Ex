c4a804e11d214a3b75c092bdd57ed4bb4c3c3280
Mery <maria.juan.vi@gmail.com>
2021-03-04 09:43:51 +0100

    Codigo T4

--- testing-js-ex/src/app.js
+++ testing-js-ex/src/app.js
@@ -8,6 +8,8 @@ module.exports = class GestionLibros {
             this.descuento = 5;
         }else if(cantidad == 3){
             this.descuento = 10;
+        }else if (cantidad == 4){
+            this.descuento = 20;
         }
     }
     getDescuento(){
