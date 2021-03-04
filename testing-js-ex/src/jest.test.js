03000ad6ce2e1cdbeb74ec1fe4530056678f9486
Mery <maria.juan.vi@gmail.com>
2021-03-04 09:33:48 +0100

    Test 2

--- testing-js-ex/src/jest.test.js
+++ testing-js-ex/src/jest.test.js
@@ -1,6 +1,14 @@
 var Libro = require('./libro');
+var GestionLibros = require('./app');
+
 //
 test('TDD: Coste inicial de un libro es de 8€', () => {
     libro = new Libro();
     expect(libro.getPrecio()).toBe(8);
+});
+
+test('TDD: Si se adquieren 2 libros, se aplica 5% de descuento', () => {
+    gestionLibros = new GestionLibros();
+    gestionLibros.adquirirLibros(2);
+    expect(gestionLibros.getDescuento()).toBe(5);
 });
